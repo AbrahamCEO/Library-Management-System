04b5c854d766843520a81f8c45e656e7a9cfaba2
AMN <87907130+AbrahamCEO@users.noreply.github.com>
2024-08-18 22:34:34 +0200

    Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './styles.css'; // Assuming you have some styles here
-lol
+
 const Signup = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
